7aefa358cbe7f86ce07c23a4e5fe62f5bec9d8c3
tatiananantes <tatiananantes@gmail.com>
2022-10-02 17:26:59 +0100

    first routes register, show, index, added and working in Postman

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,40 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
   
-end
+  #GET /users - all users
+  def index
+    @users = User.all
+    render json: @users, status: :ok
+  end
+
+  #GET /users/{username} - shows a specific user
+  def show
+    render json: @user, status: :ok
+  end
+
+  #POST /users - creates a user
+  def register
+    @user = User.new(user_params)
+    if @user.save
+      response = { message: 'User has been successfully created!'}
+      render json: response, status: :created
+    else 
+      render json: @user.errors, status: :bad
+    end
+  end
+
+  private
+
+    def user_params
+      params.permit(
+        :username,
+        :email,
+        :password
+      )
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+ 
+  #index all users
+  get '/users', to: 'users#index'
+
+  #show route - specific user
+  get '/users/:id', to: 'users#show', as: 'user'
+  
+  #user registration route
+  post 'auth/register', to: 'users#register'
+
 end
