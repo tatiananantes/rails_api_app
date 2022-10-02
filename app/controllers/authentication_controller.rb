ae500016ebc33abe729fc016cf7975e7f1a29cf8
tatiananantes <tatiananantes@gmail.com>
2022-10-02 19:43:58 +0100

    log in with username and password instead of email and password as per requirements

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,7 +3,7 @@ class AuthenticationController < ApplicationController
 
   #POST /auth/login
   def login
-    @user = User.find_by_email(params[:email])
+    @user = User.find_by_username(params[:username])
     if @user&.authenticate(params[:password])
       token = jwt_encode(user_id: @user.id)
       render json: { token: token }, status: :ok
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
+  skip_before_action :authenticate_request, only: %i[login register]
   
   #GET /users - all users
   def index
